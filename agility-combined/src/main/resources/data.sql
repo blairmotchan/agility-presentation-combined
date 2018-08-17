ecf176c715b8f87e0e2a53ce2ba2987b0898eb4b
Jon Haarmann <jhaarmann@labelinsight.com>
2018-08-17 09:16:26 -0500

    Registration data seeding

--- agility-combined/src/main/resources/data.sql
+++ agility-combined/src/main/resources/data.sql
@@ -7,3 +7,13 @@ INSERT into attendee values (4, 'Katie');
 INSERT into session values (1, 'Spring');
 INSERT into session values (2, 'React');
 INSERT into session values (3, 'NiFi');
+
+INSERT into attendee_session values (1, 1, 1);
+INSERT into attendee_session values (2, 1, 2);
+INSERT into attendee_session values (3, 1, 3);
+INSERT into attendee_session values (4, 2, 2);
+INSERT into attendee_session values (5, 3, 1);
+INSERT into attendee_session values (6, 3, 3);
+INSERT into attendee_session values (7, 4, 1);
+INSERT into attendee_session values (8, 4, 2);
+INSERT into attendee_session values (9, 4, 3);
