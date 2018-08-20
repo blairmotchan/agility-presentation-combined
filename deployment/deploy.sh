92008e421d7c1156d40c459972653b145b8285d4
Blair Motchan <bmotchan@labelinsight.com>
2018-08-20 06:44:55 -0500

    completing circle

---
+++ deployment/deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+ssh-keyscan  $DEPLOYMENT_SERVER >> ~/.ssh/known_hosts
+scp -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 \
+    agility-attendee-sessions/target/agility-attendee-session.jar \
+    $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:/home/$DEPLOYMENT_USER
+scp -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 \
+    agility-combined/target/agility-combined.jar \
+    $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:/home/$DEPLOYMENT_USER
+scp -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 \
+    agility-registration/target/agility-registration.jar \
+    $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:/home/$DEPLOYMENT_USER
+
+ssh -i id_rsa_ab7c42aebeba55bc1d50f35b575e2325 'java -jar repo/agility-attendee-sessions/target/agility-attendee-session.jar >> agility-attendee.log & java -jar repo/agility-combined/target/agility-combined.jar >> agility-combined.log & java -jar repo/agility-registration/target/agility-registration.jar >> agility-registration.log'
