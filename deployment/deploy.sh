ef4216014efb6135000b36c26b18053f8b9ab6e0
Blair Motchan <blair.motchan@gmail.com>
2018-08-20 06:54:20 -0500

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -11,4 +11,4 @@ scp -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 \
     agility-registration/target/agility-registration.jar \
     $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:/home/$DEPLOYMENT_USER
 
-ssh -i id_rsa_ab7c42aebeba55bc1d50f35b575e2325 'java -jar agility-attendee-sessions/target/agility-attendee-session.jar >> agility-attendee.log & java -jar agility-combined/target/agility-combined.jar >> agility-combined.log & java -jar agility-registration/target/agility-registration.jar >> agility-registration.log'
+ssh -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 'java -jar agility-attendee-sessions/target/agility-attendee-session.jar >> agility-attendee.log & java -jar agility-combined/target/agility-combined.jar >> agility-combined.log & java -jar agility-registration/target/agility-registration.jar >> agility-registration.log'
