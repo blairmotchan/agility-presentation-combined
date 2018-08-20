ebe9af5bbfae5effe05390498efa76940cbaf258
Blair Motchan <blair.motchan@gmail.com>
2018-08-20 07:43:27 -0500

    finally fixing deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -11,4 +11,4 @@ scp -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 \
     agility-registration/target/agility-registration.jar \
     $DEPLOYMENT_USER@$DEPLOYMENT_SERVER:/home/$DEPLOYMENT_USER
 
-ssh -i id_rsa_ab7c42aebeba55bc1d50f35b575e2325 'kill -9 $(lsof -t -i:8081) & kill -9 $(lsof -t -i:8082) kill -9 $(lsof -t -i:8083) & /usr/bin/java -jar repo/agility-attendee-sessions/target/agility-attendee-session.jar >> agility-attendee.log & /usr/bin/java -jar repo/agility-combined/target/agility-combined.jar >> agility-combined.log & /usr/bin/java -jar repo/agility-registration/target/agility-registration.jar >> agility-registration.log'
+ssh -i ~/.ssh/id_rsa_ab7c42aebeba55bc1d50f35b575e2325 $DEPLOYMENT_USER@$DEPLOYMENT_SERVER 'kill -9 $(lsof -t -i:8081) & kill -9 $(lsof -t -i:8082) kill -9 $(lsof -t -i:8083) & /usr/bin/java -jar agility-attendee-session.jar >> agility-attendee.log & /usr/bin/java -jar agility-combined.jar >> agility-combined.log & /usr/bin/java -jar agility-registration.jar >> agility-registration.log &'
